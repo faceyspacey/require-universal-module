a01157f21eb2bd4f69e8b99befeb6b1a423605db
James Gillmore <james@faceyspacey.com>
2017-06-03 17:20:34 -0700

    fix($test): make first test work so coverage reporter can (hopefully) work

--- __tests__/index.js
+++ __tests__/index.js
@@ -1,3 +1,10 @@
-test('first', () => {
+import requireUniversalModule from '../src'
 
+test('first', () => {
+  const { requireSync, requireAsync, addModule, mod } = requireUniversalModule(
+    () => Promise.resolve(null),
+    {}
+  )
+
+  expect(mod).toBeFalsy()
 })
