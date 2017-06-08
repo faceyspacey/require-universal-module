e74ccda807879f3877479ad0a054b49666381e83
James Gillmore <james@faceyspacey.com>
2017-06-08 02:02:12 -0700

    fix(test): added test for asimcImport as function with args

--- __tests__/index.js
+++ __tests__/index.js
@@ -123,6 +123,13 @@ describe('requireAsync: requires module asynchronously on the client, returning 
     }
   })
 
+  it('asyncImport as function with args: (cb, a, b) => import()', async () => {
+    const { requireAsync } = req((cb, a, b) => Promise.resolve([a, b]))
+
+    const res = await requireAsync(1, 2)
+    expect(res).toEqual([1, 2])
+  })
+
   it('return Promise.resolve(mod) if module already synchronously required', async () => {
     const modulePath = createPath('es6')
     const { requireAsync, mod } = req(undefined, { path: modulePath })
